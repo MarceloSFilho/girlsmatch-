a6c5dfa2e3bffd779a107102ef5f0a3d8080a3d9
Tainan Bogo <tainanbogo@gmail.com>
2023-03-10 15:36:44 -0300

    chatroom done

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
   # Defines the root path route ("/")
   # root "articles#index"
   get "/users/:id", to: "users#show"
   get "mentorships", to: "mentorships#index"
   get "my_proposals", to: "mentorships#my_proposals"
   get "skills/new", to: "skills#update"
+
 end
