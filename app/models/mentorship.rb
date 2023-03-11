30983e6b0e283505c1380bf4e3476da73bafb765
Tainan Bogo <tainanbogo@gmail.com>
2023-03-11 15:54:33 -0300

    change default value of mentorship

--- app/models/mentorship.rb
+++ app/models/mentorship.rb
@@ -1,2 +1,4 @@
 class Mentorship < ApplicationRecord
+  belongs_to :student, class_name: 'User'
+  belongs_to :mentor, class_name: 'User'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_10_135333) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_11_184932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,10 +21,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_10_135333) do
   end
 
   create_table "mentorships", force: :cascade do |t|
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.bigint "student_id", null: false
     t.bigint "mentor_id", null: false
-    t.boolean "accepted"
+    t.boolean "accepted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["mentor_id"], name: "index_mentorships_on_mentor_id"
