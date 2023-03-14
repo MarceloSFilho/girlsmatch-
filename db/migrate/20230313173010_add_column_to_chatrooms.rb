24946f6695729ad66a649a7acdb5f2251f1e1921
annedeegan <anne.deegan@outlook.com>
2023-03-14 15:34:48 -0400

    chatroom

--- db/migrate/20230313173010_add_column_to_chatrooms.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToChatrooms < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :chatrooms, :student, foreign_key: { to_table: :mentorships }
-    add_reference :chatrooms, :mentor, foreign_key: { to_table: :mentorships }
-  end
-end
