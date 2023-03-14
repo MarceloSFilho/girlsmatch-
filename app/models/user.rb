5692629789ef4568904f679616a41d73fb2a4ca9
annedeegan <anne.deegan@outlook.com>
2023-03-14 14:48:44 -0400

    search

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ApplicationRecord
   has_many :mentor_mentorships, class_name: 'Mentorships', foreign_key: 'mentor_id'
   has_many :skills, dependent: :destroy
   has_one_attached :photo
+  include PgSearch::Model
+
+  pg_search_scope :search_by_name_and_language,
+  against: [ :username, :first_name, :last_name],
+   associated_against:
+    { skills: [ :language ] },
+    using: {
+      tsearch: { prefix: true }
+    }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+if Rails.env.development?
+  Skill.destroy_all
+  User.destroy_all
+  Chatroom.destroy_all
+
+ @joao = User.create(first_name: 'João', last_name: "pedro", email: 'joao@hotmail.com', password: '123456', mentor:true)
+ @maria = User.create(first_name: 'Maria',last_name: "joaquina", email: 'maria@hotmail.com', password: '123456', mentor: true)
+ 1.times do
+   Skill.create(language: Skill::LANGUAGES.sample, user_id: @joao.id)
+ end
+ 1.times do
+   Skill.create(language: Skill::LANGUAGES.sample, user_id: @maria.id)
+ end
+ end
