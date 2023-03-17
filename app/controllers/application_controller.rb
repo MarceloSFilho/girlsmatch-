5deefe2b49c7b7702543b45185ac0083e1cb73af
annedeegan <anne.deegan@outlook.com>
2023-03-17 12:46:26 -0400

    validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:student, :mentor, :username, :photo, :description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:student, :username, :photo, :description])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :photo, :description, :skills, skills_attributes: [:language, :id]])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_150548) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_17_141551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -90,7 +90,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_150548) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "student"
-    t.boolean "mentor"
     t.boolean "available", default: true
     t.string "first_name"
     t.string "last_name"
